0765e3fd244739a47d21d0040e0b2bd075df4ff5
Reet Awwsum <reetawwsum@yahoo.com>
2016-05-07 20:28:54 +0530

    Added to draw image of digit

---
+++ digit_plot.py
@@ -0,0 +1,34 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from digit_recognizer_input import *
+
+def draw_digit(digit, target='Unknown'):
+	plt.figure()
+	plt.title(target)	
+
+	plt.imshow(np.reshape(digit, (28, 28)), cmap=plt.cm.gray_r)
+
+	plt.xticks(())
+	plt.yticks(())
+	
+	return plt
+
+if __name__ == '__main__':
+	train, validation, test = load_digits(0.01, 0.01, 0.1)
+	X_train = train.data
+	X_validation = validation.data
+	X_test = test.data
+	y_train = train.target
+	y_validation = validation.target
+
+	num1 = np.random.randint(len(X_train))
+	num2 = np.random.randint(len(X_validation))
+	num3 = np.random.randint(len(X_test))
+
+	draw_digit(X_train[num1].astype(int), y_train[num1])
+
+	draw_digit(X_validation[num2].astype(int), y_validation[num2])
+
+	draw_digit(X_test[num3].astype(int))
+	plt.show()
+
