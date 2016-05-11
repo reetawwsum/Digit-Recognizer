89a6bfa6c55701f80077f693d77e6279aa65c9a6
Reet Awwsum <reetawwsum@yahoo.com>
2016-05-11 18:31:11 +0530

    Added three layer model (784-128-32-10)

--- digit_recognizer_model.py
+++ digit_recognizer_model.py
@@ -21,17 +21,24 @@ def bias_variable(shape):
 def inference(images):
 	# Hidden 1
 	with tf.name_scope('hidden1'):
-		weights = weight_variable([784, 800])
-		biases = bias_variable([800])
+		weights = weight_variable([784, 128])
+		biases = bias_variable([128])
 
 		hidden1 = tf.nn.relu(tf.matmul(images, weights) + biases)
 
+	# Hidden 2
+	with tf.name_scope('hidden2'):
+		weights = weight_variable([128, 32])
+		biases = bias_variable([32])
+
+		hidden2 = tf.nn.relu(tf.matmul(hidden1, weights) + biases)
+
 	# Linear Layer
 	with tf.name_scope('linear'):
-		weights = weight_variable([800, 10])
+		weights = weight_variable([32, 10])
 		biases = bias_variable([10])
 
-		logits = tf.matmul(hidden1, weights) + biases
+		logits = tf.matmul(hidden2, weights) + biases
 
 	return logits
 
