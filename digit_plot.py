bfa374d999db1d967edcb8aafef3330e7ec03bb2
Reet Awwsum <reetawwsum@yahoo.com>
2016-05-15 15:01:34 +0530

    Changes done according to return of load_digits()

--- digit_plot.py
+++ digit_plot.py
@@ -14,24 +14,20 @@ def draw_digit(digit, target='Unknown'):
 	return plt
 
 if __name__ == '__main__':
-	dataset = load_digits(0.01, 0.01, 0.1)
+	dataset = load_digits(0.01, 0.1)
 	train = dataset['train_dataset']
 	validation = dataset['validation_dataset']
-	test = dataset['test_dataset']
 	X_train = train.data
 	X_validation = validation.data
-	X_test = test.data
 	y_train = train.target
 	y_validation = validation.target
 
 	num1 = np.random.randint(len(X_train))
 	num2 = np.random.randint(len(X_validation))
-	num3 = np.random.randint(len(X_test))
 
 	draw_digit(X_train[num1].astype(int), y_train[num1])
 
 	draw_digit(X_validation[num2].astype(int), y_validation[num2])
 
-	draw_digit(X_test[num3].astype(int))
 	plt.show()
 
